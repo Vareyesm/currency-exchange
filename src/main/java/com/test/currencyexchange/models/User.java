50afbd2cc64517f427e3ee540aa5acc59e271110
Alfredo Reyes <alfredor.developer@gmail.com>
2022-03-05 07:42:39 -0500

    Implementación de seguridad jwt
    Manejo de seguridad por roles y perfiles

---
+++ src/main/java/com/test/currencyexchange/models/User.java
@@ -0,0 +1,39 @@
+package com.test.currencyexchange.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(	name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "profile",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
